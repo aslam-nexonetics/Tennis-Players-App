501df962ea3d63fa8ef84e248bb1c512965868a1
aslam-nexonetics <aslam-nexonetics@users.noreply.github.com>
2026-08-07 08:54:53 +0000

    deploy: df9972f5ccfaa9d054d085946eeaa14827797783

--- previews/feature-new-db-integration/main.dart.js
+++ previews/feature-new-db-integration/main.dart.js
@@ -28641,7 +28641,7 @@ $S:2}
 A.ayR.prototype={
 $1(a){var s=A.di().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/0cd610717bde95fd88343c64f81c11ba4e5c0010/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/5a2a6a42cce67f965cf540fcecf616faca624aa1/":s)+a},
 $S:74}
 A.NA.prototype={
 gB(a){var s=this.a
